edfdfbd9182aa11f2097874fd9e4d7314edc5975
Tazo Jincharadze <tazojincharadze20@gmail.com>
2026-06-16 02:07:29 +0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -7,8 +7,8 @@ from models import User
 
 
 class RegisterForm(FlaskForm):
-    username         = StringField("Enter Username", validators=[DataRequired(), Length(min=3, max=32)])
-    password         = PasswordField("Enter Password", validators=[DataRequired(), Length(min=6, max=24)])
+    username = StringField("Enter Username", validators=[DataRequired(), Length(min=3, max=32)])
+    password = PasswordField("Enter Password", validators=[DataRequired(), Length(min=6, max=24)])
     confirm_password = PasswordField("Confirm Password", validators=[
         DataRequired(), EqualTo("password", message="პაროლები არ ემთხვევა")
     ])
@@ -20,28 +20,28 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username",   validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    login    = SubmitField("Log In")
+    login = SubmitField("Log In")
 
 
 class BookForm(FlaskForm):
-    image       = FileField("Upload Book Cover")
-    title       = StringField("Book Title",        validators=[DataRequired()])
-    author      = StringField("Author",            validators=[DataRequired()])
-    year        = IntegerField("Publication Year", validators=[DataRequired(), NumberRange(min=0, max=2100)])
-    genre       = SelectField("Genre", choices=[
+    image = FileField("Upload Book Cover")
+    title = StringField("Book Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    year = IntegerField("Publication Year", validators=[DataRequired(), NumberRange(min=0, max=2100)])
+    genre = SelectField("Genre", choices=[
         ("Fiction", "Fiction"), ("Non-Fiction", "Non-Fiction"),
         ("Science Fiction", "Science Fiction"), ("Fantasy", "Fantasy"),
         ("Mystery", "Mystery"), ("Biography", "Biography"),
         ("History", "History"), ("Other", "Other"),
     ])
     description = TextAreaField("Description")
-    submit      = SubmitField("Save Book")
+    submit = SubmitField("Save Book")
 
 
 class RatingForm(FlaskForm):
-    score  = SelectField("Your Rating", choices=[
+    score = SelectField("Your Rating", choices=[
         (1, "⭐ 1"), (2, "⭐⭐ 2"), (3, "⭐⭐⭐ 3"),
         (4, "⭐⭐⭐⭐ 4"), (5, "⭐⭐⭐⭐⭐ 5")
     ], coerce=int, validators=[DataRequired()])
@@ -49,10 +49,10 @@ class RatingForm(FlaskForm):
 
 
 class SearchForm(FlaskForm):
-    q      = StringField("Search", validators=[DataRequired()])
+    q = StringField("Search", validators=[DataRequired()])
     submit = SubmitField("Search")
 
 
 class CommentForm(FlaskForm):
-    text   = TextAreaField("Comment", validators=[DataRequired(), Length(min=1, max=1000)])
+    text = TextAreaField("Comment", validators=[DataRequired(), Length(min=1, max=1000)])
     submit = SubmitField("Post Comment")
