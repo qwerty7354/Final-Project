bb8a3eb6269f2d3a9f81aa8d4ae402d08a099026
Tazo Jincharadze <tazojincharadze20@gmail.com>
2026-06-16 02:07:08 +0400

    Update models.py

--- models.py
+++ models.py
@@ -21,12 +21,12 @@ class BaseModel:
 class User(db.Model, BaseModel, UserMixin):
     __tablename__ = "users"
 
-    id       = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-    role     = db.Column(db.String(), default="Guest")
+    role = db.Column(db.String(), default="Guest")
 
-    ratings  = db.relationship("Rating",  backref="user", lazy=True)
+    ratings = db.relationship("Rating", backref="user", lazy=True)
     comments = db.relationship("Comment", backref="user", lazy=True)
 
 
@@ -38,23 +38,23 @@ def load_user(user_id):
 class Book(db.Model, BaseModel):
     __tablename__ = "books"
 
-    id          = db.Column(db.Integer(), primary_key=True)
-    title       = db.Column(db.String(),  nullable=False)
-    author      = db.Column(db.String(),  nullable=False)
-    year        = db.Column(db.Integer(), nullable=False)
-    genre       = db.Column(db.String(),  nullable=False)
-    description = db.Column(db.Text(),    default="")
-    image       = db.Column(db.String(),  default="default_image.jpg")
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    author = db.Column(db.String(), nullable=False)
+    year = db.Column(db.Integer(), nullable=False)
+    genre = db.Column(db.String(), nullable=False)
+    description = db.Column(db.Text(), default="")
+    image = db.Column(db.String(), default="default_image.jpg")
 
-    ratings  = db.relationship("Rating",  backref="book", lazy=True)
+    ratings = db.relationship("Rating", backref="book", lazy=True)
     comments = db.relationship("Comment", backref="book", lazy=True)
 
 
 class Rating(db.Model, BaseModel):
     __tablename__ = "ratings"
 
-    id      = db.Column(db.Integer(), primary_key=True)
-    score   = db.Column(db.Integer(), nullable=False)
+    id = db.Column(db.Integer(), primary_key=True)
+    score = db.Column(db.Integer(), nullable=False)
     book_id = db.Column(db.Integer(), ForeignKey("books.id"), nullable=False)
     user_id = db.Column(db.Integer(), ForeignKey("users.id"), nullable=False)
 
@@ -62,8 +62,8 @@ class Rating(db.Model, BaseModel):
 class Comment(db.Model, BaseModel):
     __tablename__ = "comments"
 
-    id         = db.Column(db.Integer(),  primary_key=True)
-    text       = db.Column(db.Text(),     nullable=False)
+    id = db.Column(db.Integer(), primary_key=True)
+    text = db.Column(db.Text(), nullable=False)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
-    book_id    = db.Column(db.Integer(),  ForeignKey("books.id"), nullable=False)
-    user_id    = db.Column(db.Integer(),  ForeignKey("users.id"), nullable=False)
+    book_id = db.Column(db.Integer(), ForeignKey("books.id"), nullable=False)
+    user_id = db.Column(db.Integer(), ForeignKey("users.id"), nullable=False)
